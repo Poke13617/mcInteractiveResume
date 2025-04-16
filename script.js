f2fe78110d589d3c19fdfc89c931eb3e26df21e7
Poke13617 <cte-mason.curtis@sllboces.org>
2025-04-16 16:01:30 +0000

    scripts for hide/show

--- script.js
+++ script.js
@@ -6,17 +6,22 @@ const seconds = now.getSeconds();
 
 console.log(`Current time: ${hours}:${minutes}:${seconds}`);
 if(hours > 16) {
-    alert("Good Evening, welcome to my website!")
+    alert("Good Evening, welcome to my resume!");
 } else if(hours == 12) {
-    alert("Good Afternon, welcome to my website!")
+    alert("Good Afternoon, welcome to my resume!");
 } else if(hours < 12) {
-    alert("Good morning, welcome to my website")
+    alert("Good morning, welcome to my resume!");
 }
 
-
-
+ 
+$(".list1").ready(function(){
+    $(".list1").hide()
+})
 $(".skillAnim").click(function(){
-
-
+    if($(".list1").is(":hidden")){
+    $(".list1").show("slow")
+    } else {
+    $(".list1").hide("slow")
+    }
     //I need the .list1 to disappear by default and reappear
 })
